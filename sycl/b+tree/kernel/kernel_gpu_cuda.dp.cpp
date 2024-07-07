6d25656832835704455d1a61bfedaca9e8f541cb
Laranga22 <tamaralaranga@gmail.com>
2024-07-07 05:13:55 +0200

    [bfs] SYCL migration

---
+++ sycl/b+tree/kernel/kernel_gpu_cuda.dp.cpp
@@ -0,0 +1,80 @@
+#include <sycl/sycl.hpp>
+#include <dpct/dpct.hpp>
+//========================================================================================================================================================================================================200
+//	findK function
+//========================================================================================================================================================================================================200
+
+void 
+findK(	long height,
+		knode *knodesD,
+		long knodes_elem,
+		record *recordsD,
+
+		long *currKnodeD,
+		long *offsetD,
+		int *keysD, 
+		record *ansD,
+		const sycl::nd_item<3> &item_ct1)
+{
+
+	// private thread IDs
+        int thid = item_ct1.get_local_id(2);
+        int bid = item_ct1.get_group(2);
+
+        // processtree levels
+	int i;
+	for(i = 0; i < height; i++){
+
+		// if value is between the two keys
+		if((knodesD[currKnodeD[bid]].keys[thid]) <= keysD[bid] && (knodesD[currKnodeD[bid]].keys[thid+1] > keysD[bid])){
+			// this conditional statement is inserted to avoid crush due to but in original code
+			// "offset[bid]" calculated below that addresses knodes[] in the next iteration goes outside of its bounds cause segmentation fault
+			// more specifically, values saved into knodes->indices in the main function are out of bounds of knodes that they address
+			if(knodesD[offsetD[bid]].indices[thid] < knodes_elem){
+				offsetD[bid] = knodesD[offsetD[bid]].indices[thid];
+			}
+		}
+                /*
+		DPCT1118:3: SYCL group functions and algorithms
+                 * must be encountered in converged control flow. You may need
+                 * to adjust the code.
+		*/
+                /*
+		DPCT1065:10: Consider replacing
+                 * sycl::nd_item::barrier() with
+                 * sycl::nd_item::barrier(sycl::access::fence_space::local_space)
+                 * for better performance if there is no access to global
+                 * memory.
+		*/
+                item_ct1.barrier();
+
+                // set for next tree level
+		if(thid==0){
+			currKnodeD[bid] = offsetD[bid];
+		}
+                /*
+		DPCT1118:4: SYCL group functions and algorithms
+                 * must be encountered in converged control flow. You may need
+                 * to adjust the code.
+		*/
+                /*
+		DPCT1065:11: Consider replacing
+                 * sycl::nd_item::barrier() with
+                 * sycl::nd_item::barrier(sycl::access::fence_space::local_space)
+                 * for better performance if there is no access to global
+                 * memory.
+		*/
+                item_ct1.barrier();
+        }
+
+	//At this point, we have a candidate leaf node which may contain
+	//the target record.  Check each key to hopefully find the record
+	if(knodesD[currKnodeD[bid]].keys[thid] == keysD[bid]){
+		ansD[bid].value = recordsD[knodesD[currKnodeD[bid]].indices[thid]].value;
+	}
+
+}
+
+//========================================================================================================================================================================================================200
+//	End
+//========================================================================================================================================================================================================200
---
+++ sycl/b+tree/util/cuda/cuda.dp.cpp
@@ -0,0 +1,103 @@
+#ifdef __cplusplus
+#include <sycl/sycl.hpp>
+#include <dpct/dpct.hpp>
+extern "C" {
+#endif
+
+//===============================================================================================================================================================================================================200
+//	SET_DEVICE CODE
+//===============================================================================================================================================================================================================200
+
+//======================================================================================================================================================150
+//	INCLUDE/DEFINE
+//======================================================================================================================================================150
+
+#include "cuda.h"					// (in library path specified to compiler)
+
+//======================================================================================================================================================150
+//	FUNCTIONS
+//======================================================================================================================================================150
+
+//====================================================================================================100
+//	SET DEVICE
+//====================================================================================================100
+
+void setdevice(void){
+
+	// variables
+	int num_devices;
+	int device;
+
+	// work
+        num_devices = dpct::dev_mgr::instance().device_count();
+        if (num_devices > 1) {
+		
+		// variables
+		int max_multiprocessors; 
+		int max_device;
+                dpct::device_info properties;
+
+                // initialize variables
+		max_multiprocessors = 0;
+		max_device = 0;
+		
+		for (device = 0; device < num_devices; device++) {
+                        dpct::get_device_info(
+                            properties,
+                            dpct::dev_mgr::instance().get_device(device));
+                        if (max_multiprocessors < properties.get_max_compute_units()) {
+                                max_multiprocessors = properties.get_max_compute_units();
+                                max_device = device;
+			}
+		}
+                /*
+		DPCT1093:12: The "max_device" device may be not
+                 * the one intended for use. Adjust the selected device if
+                 * needed.
+		*/
+                dpct::select_device(max_device);
+        }
+
+}
+
+//====================================================================================================100
+//	GET LAST ERROR
+//====================================================================================================100
+
+void checkCUDAError(const char *msg)
+{
+        /*
+	DPCT1010:15: SYCL uses exceptions to report errors and does not
+         * use the error codes. The call was replaced with 0. You need to
+         * rewrite this code.
+	*/
+        dpct::err0 err = 0;
+        /*
+	DPCT1000:14: Error handling if-stmt was detected but could not
+         * be rewritten.
+	*/
+        if (0 != err) {
+                // fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString( err) );
+                /*
+		DPCT1009:16: SYCL uses exceptions to report
+                 * errors and does not use the error codes. The call was
+                 * replaced by a placeholder string. You need to rewrite this
+                 * code.
+		*/
+                printf("Cuda error: %s: %s.\n", msg, "<Placeholder string>");
+                /*
+		DPCT1001:13: The statement could not be
+                 * removed.
+		*/
+                fflush(NULL);
+                exit(EXIT_FAILURE);
+	}
+}
+
+//===============================================================================================================================================================================================================200
+//	END
+//===============================================================================================================================================================================================================200
+
+#ifdef __cplusplus
+}
+#endif
