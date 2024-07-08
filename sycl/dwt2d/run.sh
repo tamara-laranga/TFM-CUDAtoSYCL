239a4c7a7130937aa13199614ae417a3a1f18590
Laranga22 <tamaralaranga@gmail.com>
2024-07-08 17:34:16 +0200

    'run' files updated

--- sycl/dwt2d/run.sh
+++ sycl/dwt2d/run.sh
@@ -1,3 +1,3 @@
-SYCL_DEVICE_FILTER=cuda SYCL_PI_TRACE=1  ./dwt2d 192.bmp -d 192x192 -f -5 -l 3
+ONEAPI_DEVICE_SELECTOR=cuda:* SYCL_PI_TRACE=1  ./dwt2d 192.bmp -d 192x192 -f -5 -l 3
 ls
-SYCL_DEVICE_FILTER=cuda SYCL_PI_TRACE=1  ./dwt2d rgb.bmp -d 1024x1024 -f -5 -l 3
+ONEAPI_DEVICE_SELECTOR=cuda:* SYCL_PI_TRACE=1  ./dwt2d rgb.bmp -d 1024x1024 -f -5 -l 3
